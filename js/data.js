d28744adfc0eb3ab9bd11b4173308932c0e18aed
Valentin Silva <valentinvsilva@gmail.com>
2025-08-31 17:42:48 -0300

    done

---
+++ js/data.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function(e){
+    document.getElementById('data').innerHTML = localStorage.getItem('valor')
+})
---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('buttonText').addEventListener('click', function(e) {
+let input =  document.getElementById('inputText').value
+localStorage.setItem('valor', input)
+
+})
